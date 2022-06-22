9bc933996109c8436916ab6fcd05b2957a24118e
Ann Duong <30392423+thuyanduong@users.noreply.github.com>
2022-06-21 23:59:15 -0400

    Update Buttons.js

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,8 +2,8 @@ function Buttons(){
   return (
     <div className="green-border">
       <div>
-        <button>+1</button>
         <button>-1</button>
+        <button>+1</button>
       </div>
       <button>Toggle Dark Mode</button>
     </div>
